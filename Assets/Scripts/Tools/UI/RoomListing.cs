93ecaefc8a15de9219b93a43f2ef85a97eee84b5
mackbet <89740987+mackbet@users.noreply.github.com>
2023-05-31 14:26:35 +0600

    room ui

--- Assets/Scripts/Tools/UI/RoomListing.cs
+++ Assets/Scripts/Tools/UI/RoomListing.cs
@@ -8,12 +8,14 @@ using Photon.Pun;
 public class RoomListing : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI _roomName;
+    [SerializeField] TextMeshProUGUI _playerCount;
 
     public RoomInfo RoomInfo { get; private set; }
     public void SetRoomInfo(RoomInfo roomInfo)
     {
         RoomInfo = roomInfo;
-        _roomName.text = roomInfo.Name + " (" + roomInfo.PlayerCount + "/" + roomInfo.MaxPlayers + ")";
+        _roomName.text = roomInfo.Name;
+        _playerCount.text = $"{roomInfo.PlayerCount}/{roomInfo.MaxPlayers}";
     }
 
     public void OnClick_Button()
